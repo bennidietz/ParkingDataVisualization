8408b8a975fe5aeb976512368b5e8e830de58867
PaulaScharf <paulascharf@gmx.de>
2020-12-18 22:49:55 +0100

    load car parks from api

--- main.js
+++ main.js
@@ -4,146 +4,66 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
 	attribution:
 		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
-
-const fontAwesomeIcon = L.divIcon({
+// set the icon for car parks
+const parkingIcon = L.divIcon({
 	html: '<i class="fas fa-parking fa-2x"></i>',
 	iconSize: [20, 20],
 	className: 'myDivIcon'
 });
 
-var features = { "type":"featureCollection", features: [
-		{
-			"type":"Feature",
-			"properties":{data:[10,12,16,20,25,30,30,29,13,10,7,6],title:"Injuries Due to Swan Bite by Month"},
+/**
+ * When the window is loaded the parking date is retrieved from the server
+ */
+window.onload = () => {
+	loadCarParks()
+		.then(function(carParksArray) {
+			// save the extracted trees into the global variabel, so that future access is easier
+			let carParksGeoJSON = constructGeoJSON(carParksArray);
+			L.geoJSON(carParksGeoJSON,{
+				pointToLayer: function (feature, latlng) {
+					return L.marker(latlng, {icon: parkingIcon});
+				},
+				onEachFeature: onEachFeature
+			})
+				.addTo(map);
+		});
+};
+
+
+/**
+ * Load and interpret the trees from the geoJson file.
+ * @returns promise to return a list of trees
+ */
+function loadCarParks() {
+	return new Promise(function(resolve, reject) {
+		var xhttp = new XMLHttpRequest();
+
+		xhttp.onreadystatechange = function () {
+			if (this.readyState === 4 && this.status === 200) {
+				if (this.responseText.length === 0) {
+					reject("The URL field or the content of the field is emtpy.");
+				}
+				resolve(JSON.parse(this.responseText));
+			}
+		};
+		xhttp.open("GET", "/api?type=basedata", true);
+		xhttp.send();
+	});
+}
+
+function constructGeoJSON(carParksArray) {
+	let resultingGeoJSON = {"type":"featureCollection", features: []};
+	carParksArray.forEach((carPark) => {
+		resultingGeoJSON.features.append({"type":"Feature",
+			"properties":{title:"Im a car park!"},
 			"geometry":{
 				"type":"Point",
-				"coordinates":[7.626812,51.965812]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.632062,51.964437]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7,616812,51,965688]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.616938,51.964437]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.617313,51.962688]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.622580,51.960840]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.619438,51.959437]
-			}
-		}
-		,
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.626313,51.959812]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.630062,51.960812]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.630187,51.959812]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.637812,51.957563]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.631312,51.955813]
-			}
-		}
-		,
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.632938,51.955562]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.636313,51.949687]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {data:[100,112,130,200,210,190,170,160,150,140,110,100],title:"Some Statistic"},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.639812,51.949188]
-			}
-		}
-	]};
+				"coordinates":[carPark[25],carPark[24]]
+			}})
+	})
+	return resultingGeoJSON;
+}
 
 function onEachFeature(feature, layer) {
 	layer.bindPopup(feature.properties.name);
 }
-
-L.geoJSON(features,{
-	pointToLayer: function (feature, latlng) {
-		return L.marker(latlng, {icon: fontAwesomeIcon});
-	},
-	onEachFeature: onEachFeature
-})
-	.addTo(map);
