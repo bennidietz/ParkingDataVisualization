f197d880de23dc46eaf68f7379c1c6a9eef807f8
Christian Terbeck <mail@christian-terbeck.de>
2021-01-04 12:02:18 +0100

    installed Vue, added core component and restored .env.example file

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,18 @@ global.$ = global.jQuery = require('jquery');
 window.L = require('leaflet');
 window.chart = require('chart.js');
 window.moment = require('moment');
+
+window.Vue = require('vue');
+
+$(document).ready(function() {
+  window.preferences = new Vue({
+    el: '#preferences',
+    data: {
+      view: 'analyst',
+      day: null,
+      hour: null,
+      parkingLots: null,
+      selectedParkingLot: null,
+    },
+  });
+});
