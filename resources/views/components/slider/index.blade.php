e614294622c52ae66fd89316ba431760df406597
PaulaScharf <paulascharf@gmx.de>
2021-01-04 10:30:21 +0100

    slider and map to index page

---
+++ resources/views/components/slider/index.blade.php
@@ -0,0 +1,8 @@
+<div id="day-slider" class="row align-items-center">
+    <div class="col-sm-2"><p id="value-Days"></p></div>
+    <div class="col-sm"><div id="slider-Days"></div></div>
+</div>
+<div id="hour-slider" class="row align-items-center">
+    <div class="col-sm-2"><p id="value-Hours"></p></div>
+    <div class="col-sm"><div id="slider-Hours"></div></div>
+</div>
