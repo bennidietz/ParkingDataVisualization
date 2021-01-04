40a89b2dae08f9c63d9418d2671c34a432dec7a5
PaulaScharf <paulascharf@gmx.de>
2021-01-04 10:32:33 +0100

    map fix

--- public/js/map/index.js
+++ public/js/map/index.js
@@ -1,84 +1,92 @@
-//days in a week
-var now = new Date();
-var weekShort = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-var weekLong = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+// initialise Leaflet map
+var map = L.map("map", {}).setView([51.97, 7.63], 14);
+L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+}).addTo(map);
+// set the icon for car parks
 
-var sliderDays = d3
-    .sliderBottom()
-    .min(0)
-    .max(6)
-    .step(1)
-    .width(300)
-    .tickValues([0,1,2,3,4,5,6])
-    .tickFormat(function (d) {
-        return weekShort[d];
-    })
-    .default(englishDayToGerman(now.getDay()))
-    .handle(
-        d3
-            .symbol()
-            .type(d3.symbolCircle)
-            .size(200)()
-    )
-    .on('onchange', val => {
-        d3.select('p#value-Days').text(weekLong[val]);
+/**
+ * When the window is loaded the parking data is retrieved from the server and the visualized on the map.
+ */
+window.onload = () => {
+    loadCarParks()
+        .then(function(carParksArray) {
+            // save the extracted trees into the global variabel, so that future access is easier
+            let carParksGeoJSON = constructGeoJSON(carParksArray);
+            var rainbow = new Rainbow();
+            var style = getComputedStyle(document.body);
+            // Set start and end colors
+            rainbow.setSpectrum(style.getPropertyValue('--has-capacity'), style.getPropertyValue('--no-capacity'));
+
+            // Set the min/max range
+            rainbow.setNumberRange(0, 793);
+
+            L.geoJSON(carParksGeoJSON,{
+                pointToLayer: function (feature, latlng) {
+                    let parkingIcon = L.divIcon({
+                        //TODO: the color should be set according to current percentage/amount of free parking spaces
+                        html: '<i class="fas fa-parking fa-2x" style="color:#' + rainbow.colourAt(feature.properties.capacity) + '"></i>',
+                        iconSize: [20, 20],
+                        className: 'myDivIcon'
+                    });
+                    return L.marker(latlng, {icon: parkingIcon});
+                },
+                onEachFeature: onEachFeature
+            })
+                .addTo(map);
+        });
+};
+
+
+/**
+ * Load the carparks from the server
+ * @returns promise to return a list of a list
+ */
+function loadCarParks() {
+    return new Promise(function(resolve, reject) {
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                if (this.responseText.length === 0) {
+                    reject("The URL field or the content of the field is emtpy.");
+                }
+                resolve(JSON.parse(this.responseText)["0"]);
+            }
+        };
+        xhttp.open("GET", "https://gins.christian-terbeck.de/api?type=basedata", true);
+        xhttp.send();
     });
-
-var gDays = d3
-    .select('div#slider-Days')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)');
-
-gDays.call(sliderDays);
-
-d3.select('p#value-Days').text(weekLong[sliderDays.value()]);
-
-function englishDayToGerman(dayNumber) {
-    dayNumber--;
-    if(dayNumber<0) {
-        dayNumber = 7;
-    }
-    return dayNumber;
 }
 
-// hours in a day
-var hours = d3.range(0, 24).map(function(d) {
-    if(d<10){
-        return "0"+d+":00"
-    } else {
-        return d+":00"
-    }
-});
-var sliderHours = d3
-    .sliderBottom()
-    .min(0)
-    .max(23)
-    .step(1)
-    .width(300)
-    .tickValues(d3.range(24))
-    .default(now.getHours())
-    .handle(
-        d3
-            .symbol()
-            .type(d3.symbolCircle)
-            .size(200)()
-    )
-    .on('onchange', val => {
-        d3.select('p#value-Hours').text(hours[val]);
-    });
+function constructGeoJSON(carParksArray) {
+    let resultingGeoJSON = {"type":"featureCollection", features: []};
+    let names = carParksArray[0];
+    carParksArray.forEach((carPark, i) => {
+        if (i>0) {
+            let temp = {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [carPark[25], carPark[24]]
+                }
+            }
+            names.forEach((name,j) => {
+                temp.properties[name] = carPark[j];
+            })
+            resultingGeoJSON.features.push(temp)
+        }
+    })
+    return resultingGeoJSON;
+}
 
-var gHours = d3
-    .select('div#slider-Hours')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)');
-
-gHours.call(sliderHours);
-
-d3.select('p#value-Hours').text(hours[sliderHours.value()]);
-
+/**
+ * bind a popup to a given feature
+ * @param feature
+ * @param layer
+ */
+function onEachFeature(feature, layer) {
+    layer.bindPopup(feature.properties.name);
+}
