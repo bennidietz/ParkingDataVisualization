44e41db9748c97f3f537be828963072e2da0aa1e
Benni Dietz <b_diet05@uni-muenster.de>
2021-01-04 10:05:53 +0100

    Auto stash before merge of "main" and "origin/main"

--- resources/views/components/chart/index.blade.php
+++ resources/views/components/chart/index.blade.php
@@ -1,1 +1,13 @@
 
+<link rel="stylesheet" type="text/css" href="./barplot.css" />
+
+<label>Select the parking lot: </label>
+<select name="parkingLots" id="parkingLots"></select>
+
+<div id="plotArea">
+    <div id="barPlotArea"></div>
+    <div id="canvasBarPlot"></div>
+    <div id="piePlotArea"></div>
+</div>
+<script type="text/javascript" src="js/chart/testdata.js"></script>
+<script type="text/javascript" src="js/chart/barplot.js"></script>
