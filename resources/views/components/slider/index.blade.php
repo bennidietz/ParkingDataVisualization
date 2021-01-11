a7d4ed34792b171718e6594d30580078e898a3a6
PaulaScharf <paulascharf@gmx.de>
2021-01-11 20:41:09 +0100

    added first (new) implementation of slider

--- resources/views/components/slider/index.blade.php
+++ resources/views/components/slider/index.blade.php
@@ -1,8 +1,51 @@
-<div id="day-slider" class="row align-items-center">
-    <div class="col-sm-2"><p id="value-Days"></p></div>
-    <div class="col-sm"><div id="slider-Days"></div></div>
-</div>
-<div id="hour-slider" class="row align-items-center">
-    <div class="col-sm-2"><p id="value-Hours"></p></div>
-    <div class="col-sm"><div id="slider-Hours"></div></div>
+<div class="container">
+    <div class="row">
+        <div class="range">
+            <input type="range" min="1" max="7" steps="1" value="1" onInput="inputChange('dayslider','daylabel')" id="dayslider">
+        </div>
+
+        <ul class="range-labels" id="daylabel">
+            <li class="active selected" onclick="clicked(1,'dayslider','daylabel')">Mon</li>
+            <li onclick="clicked(2,'dayslider','daylabel')">Tue</li>
+            <li onclick="clicked(3,'dayslider','daylabel')">Wed</li>
+            <li onclick="clicked(4,'dayslider','daylabel')">Thu</li>
+            <li onclick="clicked(5,'dayslider','daylabel')">Fri</li>
+            <li onclick="clicked(6,'dayslider','daylabel')">Sat</li>
+            <li onclick="clicked(7,'dayslider','daylabel')">Sun</li>
+        </ul>
+    </div>
+    <br>
+    <div class="row">
+        <div class="range">
+            <input type="range" min="1" max="24" steps="1" value="1" onInput="inputChange('hourslider','hourlabel')" id="hourslider">
+        </div>
+
+        <ul class="range-labels" id="hourlabel">
+            <li class="active selected" onclick="clicked(1,'hourslider','hourlabel')">0</li>
+            <li onclick="clicked(2,'hourslider','hourlabel')">1</li>
+            <li onclick="clicked(3,'hourslider','hourlabel')">2</li>
+            <li onclick="clicked(4,'hourslider','hourlabel')">3</li>
+            <li onclick="clicked(5,'hourslider','hourlabel')">4</li>
+            <li onclick="clicked(6,'hourslider','hourlabel')">5</li>
+            <li onclick="clicked(7,'hourslider','hourlabel')">6</li>
+            <li onclick="clicked(8,'hourslider','hourlabel')">7</li>
+            <li onclick="clicked(9,'hourslider','hourlabel')">8</li>
+            <li onclick="clicked(10,'hourslider','hourlabel')">9</li>
+            <li onclick="clicked(11,'hourslider','hourlabel')">10</li>
+            <li onclick="clicked(12,'hourslider','hourlabel')">11</li>
+            <li onclick="clicked(13,'hourslider','hourlabel')">12</li>
+            <li onclick="clicked(14,'hourslider','hourlabel')">13</li>
+            <li onclick="clicked(15,'hourslider','hourlabel')">14</li>
+            <li onclick="clicked(16,'hourslider','hourlabel')">15</li>
+            <li onclick="clicked(17,'hourslider','hourlabel')">16</li>
+            <li onclick="clicked(18,'hourslider','hourlabel')">17</li>
+            <li onclick="clicked(19,'hourslider','hourlabel')">18</li>
+            <li onclick="clicked(20,'hourslider','hourlabel')">19</li>
+            <li onclick="clicked(21,'hourslider','hourlabel')">20</li>
+            <li onclick="clicked(22,'hourslider','hourlabel')">21</li>
+            <li onclick="clicked(23,'hourslider','hourlabel')">22</li>
+            <li onclick="clicked(24,'hourslider','hourlabel')">23</li>
+        </ul>
+    </div>
+    <br>
 </div>
