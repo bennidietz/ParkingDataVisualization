103d82586641d486c785b9e3f3117d7b484446d2
PaulaScharf <paulascharf@gmx.de>
2021-01-14 16:20:16 +0100

    added rainbowvis

--- public/js/app.js
+++ public/js/app.js
@@ -94455,8 +94455,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Christian\github\ParkingDataVisualization\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Christian\github\ParkingDataVisualization\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\paula\Documents\NotFun\Studium\Master_Geoinformatics\7.Semester\Geoinformation in Society\ParkingDataVisualization\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\paula\Documents\NotFun\Studium\Master_Geoinformatics\7.Semester\Geoinformation in Society\ParkingDataVisualization\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
