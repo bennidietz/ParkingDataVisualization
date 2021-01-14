e8b144f97b64b3752c5f2048c09048053eb20942
Benni Dietz <b_diet05@uni-muenster.de>
2021-01-14 21:01:50 +0100

    Include vue-chartjs chart (not entirely working yet)

---
+++ resources/js/components/LineChart.vue
@@ -0,0 +1,39 @@
+
+  <div>
+    <canvas></canvas>
+  </div>
+
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    },
+    selectedPLot: {
+      type: Number,
+      default: null
+    },
+    hour: {
+      type: Number,
+      default: null
+    }
+  },
+  mounted () {
+    console.log(this.selectedPLot)
+    if (this.selectedPLot) {
+      console.log(this.selectedPLot)
+      this.chartdata.backgroundColor[this.selectedPLot] = 'rgba(255, 99, 132, 1)'
+    }
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
--- resources/views/components/chart/index.blade.php
+++ resources/views/components/chart/index.blade.php
@@ -1,4 +1,4 @@
-<label>Select the parking lot: </label>
+<h2>Chart view</h2>
 <select name="parkingLots" id="parkingLots"></select>
 
 <div id="plotArea">
@@ -6,4 +6,38 @@
     <div id="canvasBarPlot"></div>
     <div id="piePlotArea"></div>
 </div>
+
+<!--
+<template>
+  <div class="container">
+    <line-chart
+      v-if="parkingLots"
+      :chartdata="[1,2,3,4,5,6]"/>
+  </div>
+</template>-->
+<line-chart :chartdata="{labels: ['', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+}" :options="{}" :selectedPLot="3" :hour="hour"/>
+
+
 <script type="text/javascript" src="js/testdata.js"></script>
