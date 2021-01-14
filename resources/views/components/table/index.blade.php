dad83dce502954395c27732e812d87b89661123d
Jannis Fröhlking <jannis.froehlking@gmail.com>
2021-01-14 16:16:03 +0100

    add index

--- resources/views/components/table/index.blade.php
+++ resources/views/components/table/index.blade.php
@@ -1,13 +1,17 @@
 <h1>Some info about this fabulous parking lot</h1>
 <div id="accordion">
-  <div class="card" v-for="parkingLot in parkingLots">
+  <div class="card" v-for="(parkingLot, index) in parkingLots">
     <div class="class=card-header" id="headingOne">
       <h5 class="mb-0">
         <button class="btn btn-link" id="buttonOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           @{{ parkingLot.name }}
+          @{{ parkingLot.capacity }}
           <i class="fas fa-wheelchair"></i>
           <i class="fas fa-charging-station"></i>
           <i class="fas fa-female"></i>
+        </button>
+        <button class="btn btn-link" id="buttonOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" @click="selectedParkingLot=index">
+          Select
         </button>
       </h5>
     </div>
