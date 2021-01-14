99d8197c2c631b6844857b09459a2fcd94eb5c8a
Jannis Fröhlking <jannis.froehlking@gmail.com>
2021-01-14 16:07:47 +0100

    Auto stash before merge of "main" and "origin/main"

--- resources/views/components/table/index.blade.php
+++ resources/views/components/table/index.blade.php
@@ -1,3 +1,20 @@
 <h1>Some info about this fabulous parking lot</h1>
-<div id="accordion"></div>
+<div id="accordion">
+  <div class="card" v-for="parkingLot in parkingLots">
+    <div class="class=card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" id="buttonOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          @{{ parkingLot.name }}
+          <i class="fas fa-wheelchair"></i>
+          <i class="fas fa-charging-station"></i>
+          <i class="fas fa-female"></i>
+        </button>
+      </h5>
+    </div>
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-mdb-parent="#accordion">
+      <div id="bodyOne" class="card-body">
+      </div>
+    </div>
+  </div>
+</div>
 <!--<script type="text/javascript" src="js/testdata.js"></script>-->
