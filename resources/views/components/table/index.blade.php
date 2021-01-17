bf71c39e06dd19020438cc662b9bcc4bcda4cc74
Jannis Fröhlking <jannis.froehlking@gmail.com>
2021-01-17 14:26:33 +0100

    Revert "Auto stash before merge of "feature/table" and "origin/feature/table""
    This reverts commit 4cdd0e8a1ed03d422f334ae584bfdd10f04474e3.

---
+++ resources/views/components/table/index.blade.php
@@ -0,0 +1,32 @@
+<div class="table__row" :class="{'active': index == selectedParkingLot}" v-for="(parkingLot, index) in parkingLots">
+  <div class="table__heading" @click="selectedParkingLot = index">@{{ parkingLot.name }}</div>
+  <table class="table__data" v-if="selectedParkingLot == index">
+    <tr>
+      <td><i class="fas fa-wheelchair"></i></td>
+      <td>@{{ parkingLot.capicity_disabled }}</td>
+    </tr>
+    <tr>
+      <td><i class="fas fa-charging-station"></i></td>
+      <td>@{{ parkingLot.capacity_electric }}</td>
+    </tr>
+    <tr>
+      <td><i class="fas fa-female"></i></td>
+      <td>@{{ parkingLot.capacity_women }}</td>
+    </tr>
+    <tr>
+      <td><i class="fas fa-ruler-vertical"></i></td>
+      <td>@{{ parkingLot.height }} m</td>
+    </tr>
+    <tr>
+      <td><i class="fas fa-euro-sign">/h</i></td>
+      <td>@{{ parkingLot.price_per_hour }}</td>
+    </tr>
+    <tr>
+      <td>Website</td>
+      <td>@{{ parkingLot.website }}</td>
+    </tr>
+  </table>
+</div>
+
+
+<script type="text/javascript" src="js/testdata.js"></script>
